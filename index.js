451f8aa3d6d380a3d60aeed6c4fdae2fe39a4d00
Piyush Tirpude <piyushtirpude@gmail.com>
2025-12-03 18:05:06 +0530

    added the showtodo and render functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let todoInput = document.querySelector(".input");
 let addTodoButton = document.querySelector(".button");
+let showTodos = document.querySelector(".todos-container");
 let todo;
 let todoList = [];
 
@@ -18,9 +19,25 @@ function uuid(){
 addTodoButton.addEventListener("click", (e) => {
   e.preventDefault();
   todo = todoInput.value;
-  console.log(todo);
-  if(todo.length > 0){
-    todoList.push({id: uuid(),todo,isCompleted:false})
+
+  if (todo.length > 0) {
+    todoList.push({ id: uuid(), todo, isCompleted: false });
   }
+  renderTodoList(todoList);
+});
+
+showTodos.addEventListener("click", (e) => {
+  let key = e.target.dataset.key;
+  todoList = todoList.map(todo => todo.id === key ? {...todo,isCompleted : !todo.isCompleted}:todo);
+  renderTodoList(todoList);
   console.log(todoList);
 });
+
+function renderTodoList(todoList) {
+  console.log(todoList);
+  showTodos.innerHTML = todoList.map(
+    ({id,todo,isCompleted}) =>
+      `<div><input id="item-${id}" type="checkox" data-key=${id}<input/> <label for="item-${id}"class="todo todo-text t-pointer ${isCompleted ? "checked-todo" : ""}">${todo} data-key=${id}<label/><button><button/><div/>`
+  );
+}
+renderTodoList(todoList);
