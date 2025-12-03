b323618b604c52ffb76dd73d97da1b9d702fb16e
Piyush Tirpude <piyushtirpude@gmail.com>
2025-12-03 18:32:09 +0530

    added localstorage functionality for todos

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let todoInput = document.querySelector(".input");
 let addTodoButton = document.querySelector(".button");
 let showTodos = document.querySelector(".todos-container");
 let todo;
-let todoList = [];
+let localData = JSON.parse(localStorage.getItem("todo"));
+let todoList = localData || [];
 
 
 //function to create unique Id//npm install uuid
@@ -24,20 +25,31 @@ addTodoButton.addEventListener("click", (e) => {
     todoList.push({ id: uuid(), todo, isCompleted: false });
   }
   renderTodoList(todoList);
+  todoInput.value ="";
 });
 
 showTodos.addEventListener("click", (e) => {
   let key = e.target.dataset.key;
+  let deltodokey = e.target.dataset.todokey;
   todoList = todoList.map(todo => todo.id === key ? {...todo,isCompleted : !todo.isCompleted}:todo);
+  todoList = todoList.filter(todo => todo.id !== deltodokey);
   renderTodoList(todoList);
+  localStorage.setItem("todo",JSON.stringify(todoList));
   console.log(todoList);
 });
 
 function renderTodoList(todoList) {
   console.log(todoList);
   showTodos.innerHTML = todoList.map(
-    ({id,todo,isCompleted}) =>
-      `<div><input id="item-${id}" type="checkox" data-key=${id}<input/> <label for="item-${id}"class="todo todo-text t-pointer ${isCompleted ? "checked-todo" : ""}">${todo} data-key=${id}<label/><button><button/><div/>`
+    ({ id, todo, isCompleted }) =>
+      `<div class=" todo relative"><input  class ="t-checkbox t-pointer "id="item-${id}" type="checkox" data-key=${id} ${
+        isCompleted ? "checked" : ""
+      }<input/> <label for="item-${id}"class="todo todo-text t-pointer ${
+        isCompleted ? "checked" : ""
+      }">${todo} data-key=${id}<label/><button class="absolute right-0" class="button cursor"> <span data-todokey = ${id} class="material-symbols-outlined">
+delete
+</span>
+<button/><div/>`
   );
 }
 renderTodoList(todoList);
